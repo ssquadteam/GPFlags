5458acb283851c4c2f0329648fdbd2d2a4eb369c
Shane Bee <shanebolenback@me.com>
2018-09-26 13:51:42 -0700

    Name/Owner Variables
    Added some owner and player variables for these 2 flags

--- src/me/ryanhamshire/GPFlags/FlagDef_EnterCommand.java
+++ src/me/ryanhamshire/GPFlags/FlagDef_EnterCommand.java
@@ -3,6 +3,9 @@ package me.ryanhamshire.GPFlags;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import me.ryanhamshire.GriefPrevention.PlayerData;
+
 
 public class FlagDef_EnterCommand extends PlayerMovementFlagDefinition
 {
@@ -15,8 +18,9 @@ public class FlagDef_EnterCommand extends PlayerMovementFlagDefinition
         if(flag == null) return true;
         
         if(flag == this.GetFlagInstanceAtLocation(lastLocation, player)) return true;
-        
-        String [] commandLines = flag.parameters.replace("%name%", player.getName()).replace("%uuid%", player.getUniqueId().toString()).split(";");
+
+        PlayerData playerData = GriefPrevention.instance.dataStore.getPlayerData(player.getUniqueId());
+        String [] commandLines = flag.parameters.replace("%owner%", playerData.lastClaim.getOwnerName()).replace("%name%", player.getName()).replace("%uuid%", player.getUniqueId().toString()).split(";");
         for(String commandLine : commandLines)
         {
             GPFlags.AddLogEntry("Entrance command: " + commandLine);
