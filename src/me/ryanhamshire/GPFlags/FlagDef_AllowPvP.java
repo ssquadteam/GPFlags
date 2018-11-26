a2026e8cd4a45591e1478d5cf28b11faf6b565f9
BuildTools <shanebolenback@me.com>
2018-11-25 16:01:44 -0800

    AllowPvP flag - fixed issue when a player goes from one PVP claim to another it was sending the message again

--- src/me/ryanhamshire/GPFlags/FlagDef_AllowPvP.java
+++ src/me/ryanhamshire/GPFlags/FlagDef_AllowPvP.java
@@ -33,6 +33,7 @@ public class FlagDef_AllowPvP extends PlayerMovementFlagDefinition
         if(lastLocation == null) return true;
         Location to = player.getLocation();
         Flag flag = this.GetFlagInstanceAtLocation(to, player);
+        if(this.GetFlagInstanceAtLocation(lastLocation, player) != null) return true;
         if(flag == null) return true;
         if(flag == this.GetFlagInstanceAtLocation(lastLocation, player)) return true;
         WorldSettings settings = this.settingsManager.Get(player.getWorld());
