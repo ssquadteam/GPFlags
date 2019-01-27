74070ccb84c2858f3f3ca8b01c7f5b22585ba220
ShaneBeee shanebolenback@me.com <shanebolenback@me.com>
2019-01-26 20:39:49 -0800

    New Util Added a new class which will house some static methods

---
+++ src/me/ryanhamshire/GPFlags/Util.java
@@ -0,0 +1,10 @@
+package me.ryanhamshire.GPFlags;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+
+public class Util {
+
+    public static HashMap<Player, Boolean> hash = new HashMap<>();
+}
