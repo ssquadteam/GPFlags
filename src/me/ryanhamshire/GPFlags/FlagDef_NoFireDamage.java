2888004207ea75fa7d8a65f255bdbdcfd5803faa
ShaneBeee shanebolenback@me.com <shanebolenback@me.com>
2019-03-10 14:37:42 -0700

    Fix fire issue

--- src/me/ryanhamshire/GPFlags/FlagDef_NoFireDamage.java
+++ src/me/ryanhamshire/GPFlags/FlagDef_NoFireDamage.java
@@ -4,15 +4,25 @@ import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
 
 public class FlagDef_NoFireDamage extends FlagDefinition {
 
     @EventHandler(priority = EventPriority.LOWEST)
-    public void onFireSpread(BlockBurnEvent e)
-    {
+    public void onFireSpread(BlockBurnEvent e) {
         Block fire = e.getBlock();
         Flag flag = this.GetFlagInstanceAtLocation(fire.getLocation(), null);
-        if(flag == null) return;
+        if (flag == null) return;
+
+        e.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onBlockIgnite(BlockIgniteEvent e) {
+        Block fire = e.getBlock();
+        Flag flag = this.GetFlagInstanceAtLocation(fire.getLocation(), null);
+        if (flag == null) return;
+        if (e.getCause() == BlockIgniteEvent.IgniteCause.FLINT_AND_STEEL) return;
 
         e.setCancelled(true);
     }
@@ -39,4 +49,5 @@ public class FlagDef_NoFireDamage extends FlagDefinition {
     {
         return new MessageSpecifier(Messages.DisableNoFireDamage);
     }
+
 }
