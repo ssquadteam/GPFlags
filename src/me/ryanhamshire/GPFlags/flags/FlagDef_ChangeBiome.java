9e4611f4bac566667df6d0ae7d18287039d4995d
ShaneBeee <shanebolenback@me.com>
2019-09-06 17:42:45 -0700

    ChangeBiome flag - changes - Change method parameters - Update methods for looping blocks

--- src/me/ryanhamshire/GPFlags/flags/FlagDef_ChangeBiome.java
+++ src/me/ryanhamshire/GPFlags/flags/FlagDef_ChangeBiome.java
@@ -4,30 +4,56 @@ import me.ryanhamshire.GPFlags.*;
 import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import me.ryanhamshire.GriefPrevention.events.ClaimDeletedEvent;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
-import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class FlagDef_ChangeBiome extends FlagDefinition {
 
-    public void changeBiome(Location greater, Location lesser, Biome biome) {
-        for (Block block : this.getBlocks(greater, lesser)) {
-            block.setBiome(biome);
+    @SuppressWarnings("deprecation")
+    private void changeBiome(Location greater, Location lesser, Biome biome) {
+        int lX = (int) lesser.getX();
+        int lZ = (int) lesser.getZ();
+        int gX = (int) greater.getX();
+        int gZ = (int) greater.getZ();
+        for (int x = lX; x < gX; x++) {
+            for (int z = lZ; z < gZ; z++) {
+                greater.getWorld().getBlockAt(x, 60, z).setBiome(biome);
+                greater.getWorld().refreshChunk(x >> 4, z >> 4);
+            }
         }
     }
 
-    public void changeBiome(Claim claim, String biome) {
+    private void changeBiome(Claim claim, Biome biome) {
         Location greater = claim.getGreaterBoundaryCorner();
         Location lesser = claim.getLesserBoundaryCorner();
+        changeBiome(greater, lesser, biome);
+    }
 
-        changeBiome(greater, lesser, Biome.valueOf(biome));
+    public void changeBiome(CommandSender sender, Claim claim, String biome) {
+        Biome b;
+        try {
+            b = Biome.valueOf(biome);
+        } catch (Exception e) {
+            sender.sendMessage(ChatColor.RED + "Invalid biome");
+            return;
+        }
+        World world = claim.getLesserBoundaryCorner().getWorld();
+        if (GPFlags.getInstance().getWorldSettingsManager().get(world).biomeBlackList.contains(biome)) {
+            if (!(sender.hasPermission("gpflags.bypass"))) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        "&cThe biome &b" + biome + " &chas been blacklisted in this world"));
+                return;
+            }
+        }
+        changeBiome(claim, b);
     }
 
     public void resetBiome(Long claimID) {
@@ -54,72 +80,6 @@ public class FlagDef_ChangeBiome extends FlagDefinition {
         Location lesser = claim.getLesserBoundaryCorner();
 
         changeBiome(greater, lesser, biome);
-
-    }
-
-    // I didn't write this - Found it online, cause I was super duper lost
-    public List<Block> getBlocks(Location greater, Location lesser) {
-
-        Location loc1 = greater;
-        loc1.setY(64);
-        Location loc2 = lesser;
-        loc2.setY(64);
-        World w = greater.getWorld();
-
-        //First of all, we create the list:
-        List<Block> blocks = new ArrayList<Block>();
-
-        //Next we will name each coordinate
-        int x1 = loc1.getBlockX();
-        int y1 = loc1.getBlockY();
-        int z1 = loc1.getBlockZ();
-
-        int x2 = loc2.getBlockX();
-        int y2 = loc2.getBlockY();
-        int z2 = loc2.getBlockZ();
-
-        //Then we create the following integers
-        int xMin, yMin, zMin;
-        int xMax, yMax, zMax;
-        int x, y, z;
-
-        //Now we need to make sure xMin is always lower then xMax
-        if (x1 > x2) { //If x1 is a higher number then x2
-            xMin = x2;
-            xMax = x1;
-        } else {
-            xMin = x1;
-            xMax = x2;
-        }
-
-        //Same with Y
-        if (y1 > y2) {
-            yMin = y2;
-            yMax = y1;
-        } else {
-            yMin = y1;
-            yMax = y2;
-        }
-
-        //And Z
-        if (z1 > z2) {
-            zMin = z2;
-            zMax = z1;
-        } else {
-            zMin = z1;
-            zMax = z2;
-        }
-
-        //Now it's time for the loop
-        for (x = xMin; x <= xMax; x++) {
-            for (y = yMin; y <= yMax; y++) {
-                for (z = zMin; z <= zMax; z++) {
-                    Block b = new Location(w, x, y, z).getBlock();
-                    blocks.add(b);
-                }
-            }
-        }
-        return blocks;
 
     }
 
