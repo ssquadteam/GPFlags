c624f71c1289959d4ef81e54b12e6cb22875f6a1
ShaneBee <shanebolenback@me.com>
2020-01-10 14:03:00 -0800

    Utils - change - Add method for removing flight from player if they aren't allowed to fly

--- src/main/java/me/ryanhamshire/GPFlags/util/Util.java
+++ src/main/java/me/ryanhamshire/GPFlags/util/Util.java
@@ -1,6 +1,14 @@
 package me.ryanhamshire.GPFlags.util;
 
+import me.ryanhamshire.GPFlags.GPFlags;
+import me.ryanhamshire.GPFlags.Messages;
+import me.ryanhamshire.GPFlags.TextMode;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
@@ -34,6 +42,36 @@ public class Util {
             rev = 0;
         }
         return maj >= major && min >= minor && rev >= revision;
+    }
+
+
+    /** Disable the flight mode of a player whom cant fly
+     * <p>This is mainly used when a player deletes their claim.</p>
+     * @param player Player to disable flight for
+     */
+    public static void disableFlight(Player player) {
+        if (player.isFlying() && !canFly(player)) {
+            Location loc = player.getLocation();
+            Block block = loc.getBlock();
+            while (block.getY() > 2 && !block.getType().isSolid() && block.getType() != Material.WATER) {
+                block = block.getRelative(BlockFace.DOWN);
+            }
+            player.setAllowFlight(false);
+            if (loc.getY() - block.getY() >= 4) {
+                GPFlags.getInstance().getPlayerListener().addFallingPlayer(player);
+            }
+            GPFlags.sendMessage(player, TextMode.Warn, Messages.ExitFlightDisabled);
+        }
+        if (player.getAllowFlight() && !canFly(player)) {
+            player.setAllowFlight(false);
+            GPFlags.sendMessage(player, TextMode.Warn, Messages.ExitFlightDisabled);
+        }
+    }
+
+    private static boolean canFly(Player player) {
+        GameMode mode = player.getGameMode();
+        return mode == GameMode.SPECTATOR || mode == GameMode.CREATIVE ||
+                player.hasPermission("gpflags.bypass.fly") || player.hasPermission("gpflags.bypass");
     }
 
 }
