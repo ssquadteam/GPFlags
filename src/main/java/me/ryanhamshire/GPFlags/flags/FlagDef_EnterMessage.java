af78dae72fcfe1ebdfb0457a6357afabe8d6d962
ShaneBee <shanebolenback@me.com>
2020-02-25 07:12:39 -0800

    Movement flags - remove some unneeded code - try to fix some performance issues (i hope)

--- src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_EnterMessage.java
+++ src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_EnterMessage.java
@@ -1,82 +1,77 @@
 package me.ryanhamshire.GPFlags.flags;
 
-import me.ryanhamshire.GPFlags.*;
-import org.bukkit.ChatColor;
+import me.ryanhamshire.GPFlags.Flag;
+import me.ryanhamshire.GPFlags.FlagManager;
+import me.ryanhamshire.GPFlags.GPFlags;
+import me.ryanhamshire.GPFlags.MessageSpecifier;
+import me.ryanhamshire.GPFlags.Messages;
+import me.ryanhamshire.GPFlags.SetFlagResult;
+import me.ryanhamshire.GPFlags.TextMode;
+import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
-
-import me.ryanhamshire.GriefPrevention.GriefPrevention;
-import me.ryanhamshire.GriefPrevention.PlayerData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public class FlagDef_EnterMessage extends PlayerMovementFlagDefinition
-{
+public class FlagDef_EnterMessage extends PlayerMovementFlagDefinition {
+
+    private String prefix;
+
+    public FlagDef_EnterMessage(FlagManager manager, GPFlags plugin) {
+        super(manager, plugin);
+        this.prefix = plugin.getFlagsDataStore().getMessage(Messages.EnterExitPrefix);
+    }
+
     @Override
-    public boolean allowMovement(Player player, Location lastLocation, Location to)
-    {
-        if(lastLocation == null) return true;
+    public boolean allowMovement(Player player, Location lastLocation, Location to) {
+        if (lastLocation == null) return true;
         Flag flag = this.GetFlagInstanceAtLocation(to, player);
-        if(flag == null) return true;
-        
-        if(flag == this.GetFlagInstanceAtLocation(lastLocation, player)) return true;
-        
-        PlayerData playerData = GriefPrevention.instance.dataStore.getPlayerData(player.getUniqueId());
+        if (flag == null) return true;
+
+        Claim claim = GriefPrevention.instance.dataStore.getClaimAt(to, true, null);
         String message = flag.parameters;
-        if(playerData.lastClaim != null)
-        {
-            message = message.replace("%owner%", playerData.lastClaim.getOwnerName()).replace("%name%", player.getName());
+        if (claim != null) {
+            message = message.replace("%owner%", claim.getOwnerName()).replace("%name%", player.getName());
         }
 
-        String prefix = ChatColor.translateAlternateColorCodes('&', new FlagsDataStore().getMessage(Messages.EnterExitPrefix));
-        GPFlags.sendMessage(player, TextMode.Info,prefix +  message);
-        
+        GPFlags.sendMessage(player, TextMode.Info, prefix + message);
         return true;
-    }
-    
-    public FlagDef_EnterMessage(FlagManager manager, GPFlags plugin)
-    {
-        super(manager, plugin);
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
         Flag flag = this.GetFlagInstanceAtLocation(player.getLocation(), player);
-        if(flag == null) return;
-        PlayerData playerData = GriefPrevention.instance.dataStore.getPlayerData(player.getUniqueId());
+        if (flag == null) return;
+        Claim claim = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), false, null);
         String message = flag.parameters;
-        message = message.replace("%owner%", playerData.lastClaim.getOwnerName()).replace("%name%", player.getName());
-        String prefix = ChatColor.translateAlternateColorCodes('&', new FlagsDataStore().getMessage(Messages.EnterExitPrefix));
+        message = message.replace("%owner%", claim.getOwnerName()).replace("%name%", player.getName());
         GPFlags.sendMessage(player, TextMode.Info, prefix + message);
     }
-    
+
     @Override
-    public String getName()
-    {
+    public String getName() {
         return "EnterMessage";
     }
 
     @Override
-    public SetFlagResult ValidateParameters(String parameters)
-    {
-        if(parameters.isEmpty())
-        {
+    public SetFlagResult ValidateParameters(String parameters) {
+        if (parameters.isEmpty()) {
             return new SetFlagResult(false, new MessageSpecifier(Messages.MessageRequired));
         }
 
         return new SetFlagResult(true, this.getSetMessage(parameters));
     }
-    
+
     @Override
-	public MessageSpecifier getSetMessage(String parameters)
-    {
+    public MessageSpecifier getSetMessage(String parameters) {
         return new MessageSpecifier(Messages.AddedEnterMessage, parameters);
     }
 
     @Override
-    public MessageSpecifier getUnSetMessage()
-    {
+    public MessageSpecifier getUnSetMessage() {
         return new MessageSpecifier(Messages.RemovedEnterMessage);
     }
+
 }
