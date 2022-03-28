2cbb455ba51325331baa7e2700b0deaee7bdcb6b
Kaan <drbot7@gmail.com>
2022-03-28 15:58:12 -0400

    verbose output now hover events

--- src/main/java/me/ryanhamshire/GPFlags/commands/ListClaimFlagsCmd.java
+++ src/main/java/me/ryanhamshire/GPFlags/commands/ListClaimFlagsCmd.java
@@ -9,10 +9,13 @@ import me.ryanhamshire.GPFlags.util.Util;
 import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import me.ryanhamshire.GriefPrevention.PlayerData;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Arrays;
 import java.util.Collection;
 
 public class ListClaimFlagsCmd extends BaseCmd {
@@ -31,60 +34,88 @@ public class ListClaimFlagsCmd extends BaseCmd {
 
         Claim claim = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), false, playerData.lastClaim);
 
-        boolean verbose = false;
-        if (args.length > 0 && args[0].toLowerCase().contains("verbose")) verbose = true;
-
         Collection<Flag> flags;
         boolean flagsFound = false;
-        StringBuilder builder1 = new StringBuilder();
-        StringBuilder builder2 = new StringBuilder();
-        StringBuilder builder3 = new StringBuilder();
+        TextComponent component1 = null;
+        TextComponent component2 = null;
+        TextComponent component3 = null;
+        TextComponent component4 = null;
+        TextComponent component5 = null;
+        TextColor componentColor;
         if (claim != null) {
             flags = PLUGIN.getFlagManager().getFlags(claim.getID().toString());
+            if (flags.size() > 0) component1 = Component.empty();
             for (Flag flag : flags) {
                 flagsFound = true;
-                builder1.append((flag.getSet() ? "+" : "-") + flag.getFlagDefinition().getName() + (verbose ? "{" + flag.parameters + "}": "")).append(" ");
+                if (flag.getSet()) componentColor = TextColor.color(5635925);
+                else componentColor = TextColor.color(16733525);
+                component1 = component1.append(Component.text(flag.getFlagDefinition().getName())
+                        .color(componentColor)
+                        .hoverEvent(HoverEvent.showText(Component.text(flag.parameters))))
+                                .append(Component.text(" "));
             }
 
             if (claim.parent != null) {
                 flags = PLUGIN.getFlagManager().getFlags(claim.parent.getID().toString());
+                if (flags.size() > 0) component2 = Component.empty();
                 for (Flag flag : flags) {
                     flagsFound = true;
-                    builder2.append((flag.getSet() ? "+" : "-") + flag.getFlagDefinition().getName() + (verbose ? "{" + flag.parameters + "}": "")).append(" ");
+                    if (flag.getSet()) componentColor = TextColor.color(5635925);
+                    else componentColor = TextColor.color(16733525);
+                    component2= component2.append(Component.text(flag.getFlagDefinition().getName())
+                                    .color(componentColor)
+                                    .hoverEvent(HoverEvent.showText(Component.text(flag.parameters))))
+                            .append(Component.text(" "));
                 }
             }
 
             flags = PLUGIN.getFlagManager().getFlags(FlagManager.DEFAULT_FLAG_ID);
+            if (flags.size() > 0) component3 = Component.empty();
             for (Flag flag2 : flags) {
                 flagsFound = true;
-                builder3.append((flag2.getSet() ? "+" : "-") + flag2.getFlagDefinition().getName() + (verbose ? "{" + flag2.parameters + "}": "")).append(" ");
+                if (flag2.getSet()) componentColor = TextColor.color(5635925);
+                else componentColor = TextColor.color(16733525);
+                component3 = component3.append(Component.text(flag2.getFlagDefinition().getName())
+                                .color(componentColor)
+                                .hoverEvent(HoverEvent.showText(Component.text(flag2.parameters))))
+                        .append(Component.text(" "));
             }
         }
 
-        StringBuilder builder4 = new StringBuilder();
         flags = PLUGIN.getFlagManager().getFlags(player.getWorld().getName());
+        if (flags.size() > 0) component4 = Component.empty();
         for (Flag flag3 : flags) {
             flagsFound = true;
-            builder4.append((flag3.getSet() ? "+" : "-") + flag3.getFlagDefinition().getName() + (verbose ? "{" + flag3.parameters + "}": "")).append(" ");
+            if (flag3.getSet()) componentColor = TextColor.color(5635925);
+            else componentColor = TextColor.color(16733525);
+            component4 = component4.append(Component.text(flag3.getFlagDefinition().getName())
+                            .color(componentColor)
+                            .hoverEvent(HoverEvent.showText(Component.text(flag3.parameters))))
+                    .append(Component.text(" "));
         }
 
-        StringBuilder builder5 = new StringBuilder();
         flags = PLUGIN.getFlagManager().getFlags("everywhere");
+        if (flags.size() > 0) component5 = Component.empty();
         for (Flag flag4 : flags) {
             flagsFound = true;
-            builder5.append((flag4.getSet() ? "+" : "-") + flag4.getFlagDefinition().getName() + (verbose ? "{" + flag4.parameters + "}": "")).append(" ");
+            if (flag4.getSet()) componentColor = TextColor.color(5635925);
+            else componentColor = TextColor.color(16733525);
+            component5 = component5.append(Component.text(flag4.getFlagDefinition().getName())
+                            .color(componentColor)
+                            .hoverEvent(HoverEvent.showText(Component.text(flag4.parameters))))
+                    .append(Component.text(" "));
         }
 
-        if (builder1.length() > 0)
-            Util.sendMessage(player, TextMode.Info, Messages.FlagsClaim, builder1.toString());
-        if (builder2.length() > 0)
-            Util.sendMessage(player, TextMode.Info, Messages.FlagsParent, builder2.toString());
-        if (builder3.length() > 0)
-            Util.sendMessage(player, TextMode.Info, Messages.FlagsDefault, builder3.toString());
-        if (builder4.length() > 0)
-            Util.sendMessage(player, TextMode.Info, Messages.FlagsWorld, builder4.toString());
-        if (builder5.length() > 0)
-            Util.sendMessage(player, TextMode.Info, Messages.FlagsServer, builder5.toString());
+        if (component1 != null)
+            Util.sendComponent(player, Messages.FlagsClaim, component1);
+        if (component2 != null)
+            Util.sendComponent(player, Messages.FlagsParent, component2);
+        if (component3 != null)
+            Util.sendComponent(player, Messages.FlagsDefault, component3);
+        if (component4 != null)
+            Util.sendComponent(player, Messages.FlagsWorld, component4);
+        if (component5 != null)
+            Util.sendComponent(player, Messages.FlagsServer, component5);
 
         if (!flagsFound) {
             Util.sendMessage(player, TextMode.Info, Messages.NoFlagsHere);
