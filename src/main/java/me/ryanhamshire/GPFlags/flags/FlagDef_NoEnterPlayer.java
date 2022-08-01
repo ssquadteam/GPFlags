c3363a9d7dd3cfac4ba8c07864bac12bec0cc1a0
Kaan <drbot7@gmail.com>
2022-08-01 12:35:03 -0500

    noenterplayer is a blacklist

--- src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_NoEnterPlayer.java
+++ src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_NoEnterPlayer.java
@@ -28,7 +28,7 @@ public class FlagDef_NoEnterPlayer extends PlayerMovementFlagDefinition {
         Flag flag = this.getFlagInstanceAtLocation(to, player);
         if (flag == null) return true;
         if (lastLocation == null || flag == this.getFlagInstanceAtLocation(lastLocation, player)) return true;
-        if (flag.parameters.toUpperCase().contains(player.getName().toUpperCase())) return true;
+        if (!flag.parameters.toUpperCase().contains(player.getName().toUpperCase())) return true;
 
         Util.sendClaimMessage(player, TextMode.Err, Messages.NoEnterPlayerMessage);
         return false;
