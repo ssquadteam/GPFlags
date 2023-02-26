a48b0b64d803fe61f15c117f9c369cc130ad4469
Kaan <drbot7@gmail.com>
2023-02-26 14:32:15 -0600

    use entity.getPassengers instead of entity.getRider

--- src/main/java/me/ryanhamshire/GPFlags/listener/EntityMoveListener.java
+++ src/main/java/me/ryanhamshire/GPFlags/listener/EntityMoveListener.java
@@ -2,19 +2,24 @@ package me.ryanhamshire.GPFlags.listener;
 
 import io.papermc.paper.event.entity.EntityMoveEvent;
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.List;
 
 public class EntityMoveListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     private void onMove(EntityMoveEvent event) {
-        Player rider = event.getEntity().getRider();
-        if (rider != null) {
-            Location locTo = event.getTo();
-            Location locFrom = event.getFrom();
-            PlayerListener.processMovement(locTo, locFrom, rider, event);
-        }
+        List<Entity> passengers = event.getEntity().getPassengers();
+        if (passengers.size() == 0) return;
+        Entity passenger = passengers.get(0);
+        if (!(passenger instanceof Player)) return;
+        Player rider = (Player) passenger;
+        Location locTo = event.getTo();
+        Location locFrom = event.getFrom();
+        PlayerListener.processMovement(locTo, locFrom, rider, event);
     }
 }
