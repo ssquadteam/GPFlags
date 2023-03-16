0fed66d2b46d4b876612e73ea08633e2514b399b
Kaan <drbot7@gmail.com>
2023-03-15 21:38:31 -0500

    trim friendlyparameters

--- src/main/java/me/ryanhamshire/GPFlags/Flag.java
+++ src/main/java/me/ryanhamshire/GPFlags/Flag.java
@@ -43,7 +43,7 @@ public class Flag {
                     builder.append(idOrName).append(" ");
                 }
             }
-            return builder.toString();
+            return builder.toString().trim();
         }
         return parameters;
     }
