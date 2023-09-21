0e15c5e24de9c5ad9ec6dfad9f98a16ff4037d81
akdukaan <46543473+akdukaan@users.noreply.github.com>
2023-09-21 12:20:06 -0500

    Update FlagDef_RaidMemberOnly.java

--- src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_RaidMemberOnly.java
+++ src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_RaidMemberOnly.java
@@ -28,7 +28,7 @@ public class FlagDef_RaidMemberOnly extends FlagDefinition {
         Flag flag = this.getFlagInstanceAtLocation(event.getRaid().getLocation(), null);
         if (flag == null) return;
         Player player = event.getPlayer();
-        Claim claim = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), false, null);
+        Claim claim = GriefPrevention.instance.dataStore.getClaimAt(event.getRaid().getLocation(), false, null);
         if (claim == null) return;
         if (!Util.canAccess(claim, player)) {
             event.setCancelled(true);
