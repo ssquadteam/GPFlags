a7ab8e9c27836073afac0b24b81199948fab26e1
Kaan <drbot7@gmail.com>
2024-02-14 21:02:38 -0600

    change clear flags on transfer claim config name and actually use it

--- src/main/java/me/ryanhamshire/GPFlags/GPFlagsConfig.java
+++ src/main/java/me/ryanhamshire/GPFlags/GPFlagsConfig.java
@@ -87,8 +87,8 @@ public class GPFlagsConfig {
             settings.noMobSpawnIgnoreSpawners = inConfig.getBoolean("World Flags." + worldName + ".NoMobSpawn Flag Ignores Spawners and Eggs", true);
             outConfig.set("World Flags." + worldName + ".NoMobSpawn Flag Ignores Spawners and Eggs", settings.noMobSpawnIgnoreSpawners);
 
-            settings.resetFlagsOnTransferClaim = inConfig.getBoolean("World Flags." + worldName + ".Reset Flags On Transfer Claim", false);
-            outConfig.set("World Flags." + worldName + ".Reset Flags On Transfer Claim", settings.resetFlagsOnTransferClaim);
+            settings.clearFlagsOnTransferClaim = inConfig.getBoolean("World Flags." + worldName + ".Reset Flags On Transfer Claim", false);
+            outConfig.set("World Flags." + worldName + ".Clear Flags On Transfer Claim", settings.clearFlagsOnTransferClaim);
 
             outConfig.options().header("GriefPrevention Flags\n" + "Plugin Version: " + plugin.getDescription().getVersion() +
                     "\nServer Version: " + plugin.getServer().getVersion() + "\n\n");
--- src/main/java/me/ryanhamshire/GPFlags/listener/ClaimTransferListener.java
+++ src/main/java/me/ryanhamshire/GPFlags/listener/ClaimTransferListener.java
@@ -3,6 +3,7 @@ package me.ryanhamshire.GPFlags.listener;
 
 import me.ryanhamshire.GPFlags.Flag;
 import me.ryanhamshire.GPFlags.GPFlags;
+import me.ryanhamshire.GPFlags.WorldSettings;
 import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.events.ClaimTransferEvent;
 import org.bukkit.event.EventHandler;
@@ -17,6 +18,9 @@ public class ClaimTransferListener implements Listener {
         Claim claim = event.getClaim();
         if (claim.isAdminClaim()) return;
         if (event.getNewOwner() == null) return;
+
+        WorldSettings settings = GPFlags.getInstance().getWorldSettingsManager().get(claim.getLesserBoundaryCorner().getWorld());
+        if (settings.clearFlagsOnTransferClaim) return;
         Collection<Flag> flags = GPFlags.getInstance().getFlagManager().getFlags(claim);
         flags.clear();
 
