439b5df88a60ee68844059f441b86a28b174f64a
QarthO <qartho@gmail.com>
2024-06-20 17:15:25 -0400

    initializing cachedClaim to null

--- src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_NoVehicle.java
+++ src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_NoVehicle.java
@@ -29,7 +29,7 @@ import java.util.List;
 
 public class FlagDef_NoVehicle extends PlayerMovementFlagDefinition {
 
-    private Claim cachedClaim;
+    private Claim cachedClaim = null;
 
     public FlagDef_NoVehicle(FlagManager manager, GPFlags plugin) {
         super(manager, plugin);
