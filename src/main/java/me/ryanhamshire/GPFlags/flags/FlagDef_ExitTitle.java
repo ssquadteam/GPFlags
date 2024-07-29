0dfb43b4d7206d47b47e2187a15ec5ebbc361caa
Nahuel Dolores <contact@nahu.me>
2024-07-29 11:26:56 +0200

    Send titles using bundled audience

--- src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_ExitTitle.java
+++ src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_ExitTitle.java
@@ -42,7 +42,7 @@ public class FlagDef_ExitTitle extends PlayerMovementFlagDefinition {
             Component.text(String.format("Owned by: %s", owner), TextColor.color(204, 204, 204)),
             Title.Times.times(Ticks.duration(10L), Ticks.duration(25L), Ticks.duration(10L))
         );
-        player.showTitle(title);
+        GPFlags.getInstance().getAdventure().player(player).showTitle(title);
     }
 
     @Override
